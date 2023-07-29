bd8b68391dd7ad40841851e02633a77789a1d653
marcabounader92 <marcabounader92@outlook.com>
2023-07-29 21:50:26 +0300

    JS: added admin add

---
+++ src/scripts/admin-add.js
@@ -0,0 +1,6 @@
+window.addEventListener('load',() => {
+    const btn_logout=document.getElementById('btn-logout');
+    btn_logout.style.display="block";
+    // document.addEventListener("DOMContentLoaded", getItems());
+
+});
