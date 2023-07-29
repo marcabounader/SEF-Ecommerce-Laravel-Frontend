e63096319dc12ebd2cb4fd83421b0091956b799f
marcabounader92 <marcabounader92@outlook.com>
2023-07-29 10:55:57 +0300

    JS: added register fetch

---
+++ src/scripts/register.js
@@ -0,0 +1,26 @@
+const btn=document.getElementById('btn-register');
+
+btn.addEventListener('click',()=>{
+    let name=document.getElementById('name');
+    let email=document.getElementById('email');
+    let password=document.getElementById('password');
+    fetch("http://localhost:8000/api/users/register",{
+        method: "POST",
+        // mode: "cors",
+        cache: "no-cache",
+        origin: "http://localhost:5500",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(getcredentials),
+    })
+    .then((response) => response.json())
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
+
+});
